2e5d9c5f14eda422340d1767e5ee4e869926dd29
Ashleigh Chun <ashleigh.chun@wework.com>
2018-09-23 19:35:14 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
 
-def countdown
+def countdown(n)
+  while n > 0
+   puts "#{n} SECOND(S)!"
+   n -= 1
+ end
+return "HAPPY NEW YEAR!"
+end
+
+
+
+def countdown_with_sleep()
+  sleep(1)
 end
